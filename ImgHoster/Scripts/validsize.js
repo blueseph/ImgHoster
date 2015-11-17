3e40fd22afface0f9a50796731a33440f7324fd9
Talros <asigoth@gmail.com>
2015-11-16 22:42:18 -0500

    added frontend validation to prevent >3MB image uploads

---
+++ ImgHoster/Scripts/validsize.js
@@ -0,0 +1,8 @@
+﻿$("#i_submit").click(function (e) {
+
+    var fsize = $("#i_file")[0].files[0].size;
+    if (fsize > 3 * 1024 * 1024) {
+        e.preventDefault();
+        alert("Maximum file size allowed is 3MB.");
+    }
+});
